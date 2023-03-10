ad3ea4f2872e443bbf4f6c8a9a72ef7fef919350
DodoPredo <ph005009004@gmail.com>
2023-03-10 18:05:47 -0300

    Initial Commit

---
+++ src/main/java/net/dodopredo/modtest/ModTestClient.java
@@ -0,0 +1,10 @@
+package net.dodopredo.modtest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
